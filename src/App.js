cbe6345e1b3e23909d3ab4f969653dad6fb7a7b5
Piyush-Gautam <piyushgautamtg@gmail.com>
2021-08-31 14:40:36 +0530

    rever-space-project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React, { useEffect, useRef } from 'react';
 import './App.css';
+import img from './Images/01.jpg';
+import VanillaTilt from 'vanilla-tilt';
+import Particle from 'react-particles-js';
+import particlesConfig from'./assets/particlesConfig.json';
 
+function Tilt(props) {
+  const { options, ...rest } = props;
+  const tilt = useRef(null);
+
+  useEffect(() => {
+    VanillaTilt.init(tilt.current, options);
+  }, [options]);
+
+  return <div ref={tilt} {...rest} />;
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const options = {
+    scale: 1.2,
+    speed: 2000,
+    max: 30,
+    glare: true,
+    "max-glare": 0.2,
+    easing:.03,
+
+  };
+  return (<>
+  
+  <Particle params={particlesConfig} className="App-particles__container" />
+      <div className="App">
+         <div className="wrapper" >
+    <Tilt options={options}>
+    <Card image={img} title="Hello Card" description="This is my first card"/>
+    </Tilt>
+    <Tilt options={options}>
+    <Card image={img} title="Hello Card" description="This is my Second card"/>
+    </Tilt>
     </div>
+    <div className="wrapper" options={options}>
+    <Tilt options={options}>
+    <Card image={img} title="Hello Card" description="This is my Third card"/>
+    </Tilt>
+    <Tilt options={options}>
+    <Card image={img} title="Hello Card" description="This is my Fourth card"/>
+    </Tilt>
+    </div>
+      </div>
+  </>
   );
 }
-
+function Card(props){
+  return(
+    <div className="card" data-tilt-glare data-tilt-max-glare="0.8">
+      <div className="card_body">
+       <img className="card_image" src={props.image} alt=""/>
+       <h2 className="card_title">{props.title}</h2>
+       <p className="card_description">{props.description}</p>
+      </div>
+      <button className="card_button">Details</button>
+      <script type="text/javascript" src="vanilla-tilt.js"></script> 
+    </div>
+  )
+}
 export default App;
